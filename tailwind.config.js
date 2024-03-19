8f99719a243d9c1f1437b28504ddd615e53a98ac
Siddharta99 <siddhartawakte@gmail.com>
2024-03-19 17:07:17 +0530

    comp

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,11 @@
 // }
 
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
